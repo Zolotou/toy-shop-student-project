2fe6922b791bdd8203a61062167934bcaaac4c94
Zolotou <vadimkarulin@gmail.com>
2023-03-05 14:43:18 +0300

    new version

--- js/cart-right.js
+++ js/cart-right.js
@@ -1,5 +1,6 @@
 //Cart
 let cartIcon = document.querySelector('.basket'); // icon Basket with items counter
+let cartOverlay = document.querySelector('.cart-right-overlay'); //серый фон на сайт при открытой корзине
 let cart = document.querySelector('.cart-right');
 let closeCart = document.querySelector('.cart-close');
 
@@ -12,10 +13,16 @@ let itemsInCart = []; //cart
 
 //Open Cart
 cartIcon.onclick = () =>{
+    cartOverlay.classList.add("transparentBcg");
     cart.classList.add("active");
 };
 //Close Cart
 closeCart.onclick = () =>{
+    cartOverlay.classList.remove("transparentBcg");
+    cart.classList.remove("active");
+};
+cartOverlay.onclick = () =>{
+    cartOverlay.classList.remove("transparentBcg");
     cart.classList.remove("active");
 };
 
@@ -150,13 +157,13 @@ function removeCartItem(event){
     var buttonClicked = event.target;
     // console.log(buttonClicked);
     //Цикл чтобы в svg кликнуть любой path и Корзина не исчезала
-    var parentClass;
+    /* var parentClass;
     if(!(buttonClicked.classList.contains("cart-remove"))){
         do{
             buttonClicked = buttonClicked.parentElement;
             parentClass = buttonClicked.classList;
         }while(!(parentClass.contains("cart-remove")));
-    };      
+    };   */    
     //Проверка что товар уже был положен в корзину
     var id = buttonClicked.dataset.id; //взять Артикул товара из аттрибута data-id кнопки .add-cart
     var inCart = itemsInCart.find(function (element) {
@@ -168,10 +175,10 @@ function removeCartItem(event){
         buttonClicked = buttonClicked.parentElement;
         parentClass = buttonClicked.classList;
     }while(!(parentClass.contains("cart-right-box"))); */
-    buttonClicked.parentElement.remove();
+    buttonClicked.parentElement.parentElement.remove(); //кнопка .cart-remove обернута в form тэг
     updateTotal();      
     // Раздизэйблить кнопки "В корзину" 
-    console.log(id);   
+    // console.log(id);   
     var buttonAddToCart = document.querySelector(`.add-cart[data-id=${id}]`);    
     buttonAddToCart.innerText = "В корзину";
     buttonAddToCart.disabled = false;
@@ -256,14 +263,16 @@ function increaseQuantity(event){
 //Add to Cart
 function addCartClicked(event){
     var button = event.target;
-    var id = button.dataset.id; //взять Артикул товара из аттрибута data-id кнопки .add-cartvar id = button.dataset.id;
-    var shopProducts = button.parentElement;
+    var id = button.dataset.id; //взять Артикул товара из аттрибута data-id кнопки .add-cart var id = button.dataset.id;
+    var formWithButton = button.parentElement; //кнопка .add-cart теперь обернута в форму
+    var shopProducts = formWithButton.parentElement;
     var aTitle = shopProducts.getElementsByClassName('link-img-product')[0];
     var title = aTitle.getElementsByClassName('name-product')[0].innerText;
     // console.log(title);
 
     var divPrice = shopProducts.getElementsByClassName('price-product')[0];
-    var price = divPrice.getElementsByClassName('final-price-product')[0].innerText;
+    var finalPrice = divPrice.getElementsByClassName('final-price-product')[0];
+    var price = finalPrice.getElementsByClassName('final-price-product-amount')[0].innerText;
     // console.log(price);
 
     var divProductImg = aTitle.getElementsByClassName('img-product')[0];
@@ -271,6 +280,7 @@ function addCartClicked(event){
     // console.log(productImg);
     addProductToCart(id, title, price, productImg);
     // РОМАН: Открывает правую панель корзины при добавлении товара в корзину
+    cartOverlay.classList.add("transparentBcg");
     cart.classList.add("active");
     updateTotal();
 
@@ -316,26 +326,31 @@ function addProductToCart(id, title, price, productImg){
                         <img src="${productImg}" alt="" class="cart-img">
                         <div class="detail-box">
                             <div class="cart-product-title">${title}</div>
-                            <div class="cart-price">${price}</div>
-                            <div class="cart-quantity-controls">
-                                <button class="cart-minus-quantity" data-id="${id}">-</button>
-                                <input type="number" value="1" class="cart-quantity" data-id="${id}">
-                                <button class="cart-plus-quantity" data-id="${id}">+</button>                                
-                            </div>
+                            <div class="cart-price"><span class="cart-price-amount">${price}</span><span class="cart-price-currency"> р.</span></div>
+                            <form action="" method="post" class="">
+                                <div class="cart-quantity-controls">
+                                    <button class="cart-minus-quantity" data-id="${id}">-</button>
+                                    <input type="number" value="1" class="cart-quantity" data-id="${id}">
+                                    <button class="cart-plus-quantity" data-id="${id}">+</button>                                
+                                </div>
+                            </form>
                         </div>
                         <!--Remove cart-->
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="30" height="30" viewBox="0 0 256 256" xml:space="preserve" class="cart-remove" data-id="${id}">
-                            <defs>
-                            </defs>
-                            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
-                                <path d="M 64.71 90 H 25.291 c -4.693 0 -8.584 -3.67 -8.859 -8.355 l -3.928 -67.088 c -0.048 -0.825 0.246 -1.633 0.812 -2.234 c 0.567 -0.601 1.356 -0.941 2.183 -0.941 h 59.002 c 0.826 0 1.615 0.341 2.183 0.941 c 0.566 0.601 0.86 1.409 0.813 2.234 l -3.928 67.089 C 73.294 86.33 69.403 90 64.71 90 z M 18.679 17.381 l 3.743 63.913 C 22.51 82.812 23.771 84 25.291 84 H 64.71 c 1.52 0 2.779 -1.188 2.868 -2.705 l 3.742 -63.914 H 18.679 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                <path d="M 80.696 17.381 H 9.304 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 h 71.393 c 1.657 0 3 1.343 3 3 S 82.354 17.381 80.696 17.381 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                <path d="M 58.729 17.381 H 31.271 c -1.657 0 -3 -1.343 -3 -3 V 8.789 C 28.271 3.943 32.214 0 37.061 0 h 15.879 c 4.847 0 8.789 3.943 8.789 8.789 v 5.592 C 61.729 16.038 60.386 17.381 58.729 17.381 z M 34.271 11.381 h 21.457 V 8.789 C 55.729 7.251 54.478 6 52.939 6 H 37.061 c -1.538 0 -2.789 1.251 -2.789 2.789 V 11.381 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                <path d="M 58.33 74.991 c -0.06 0 -0.118 -0.002 -0.179 -0.005 c -1.653 -0.097 -2.916 -1.517 -2.819 -3.171 l 2.474 -42.244 c 0.097 -1.655 1.508 -2.933 3.171 -2.819 c 1.653 0.097 2.916 1.516 2.819 3.17 l -2.474 42.245 C 61.229 73.761 59.906 74.991 58.33 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                <path d="M 31.669 74.991 c -1.577 0 -2.898 -1.23 -2.992 -2.824 l -2.473 -42.245 c -0.097 -1.654 1.165 -3.073 2.819 -3.17 c 1.646 -0.111 3.073 1.165 3.17 2.819 l 2.473 42.244 c 0.097 1.654 -1.165 3.074 -2.819 3.171 C 31.788 74.989 31.729 74.991 31.669 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                <path d="M 45 74.991 c -1.657 0 -3 -1.343 -3 -3 V 29.747 c 0 -1.657 1.343 -3 3 -3 c 1.657 0 3 1.343 3 3 v 42.244 C 48 73.648 46.657 74.991 45 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                            </g>
-                        </svg>`;
+                        <form action="" method="post" class="">
+                            <button class="cart-remove" data-id="${id}"></button>
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="30" height="30" viewBox="0 0 256 256" xml:space="preserve" class="cart-remove" data-id="${id}">
+                                <defs>
+                                </defs>
+                                <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                                    <path d="M 64.71 90 H 25.291 c -4.693 0 -8.584 -3.67 -8.859 -8.355 l -3.928 -67.088 c -0.048 -0.825 0.246 -1.633 0.812 -2.234 c 0.567 -0.601 1.356 -0.941 2.183 -0.941 h 59.002 c 0.826 0 1.615 0.341 2.183 0.941 c 0.566 0.601 0.86 1.409 0.813 2.234 l -3.928 67.089 C 73.294 86.33 69.403 90 64.71 90 z M 18.679 17.381 l 3.743 63.913 C 22.51 82.812 23.771 84 25.291 84 H 64.71 c 1.52 0 2.779 -1.188 2.868 -2.705 l 3.742 -63.914 H 18.679 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    <path d="M 80.696 17.381 H 9.304 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 h 71.393 c 1.657 0 3 1.343 3 3 S 82.354 17.381 80.696 17.381 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    <path d="M 58.729 17.381 H 31.271 c -1.657 0 -3 -1.343 -3 -3 V 8.789 C 28.271 3.943 32.214 0 37.061 0 h 15.879 c 4.847 0 8.789 3.943 8.789 8.789 v 5.592 C 61.729 16.038 60.386 17.381 58.729 17.381 z M 34.271 11.381 h 21.457 V 8.789 C 55.729 7.251 54.478 6 52.939 6 H 37.061 c -1.538 0 -2.789 1.251 -2.789 2.789 V 11.381 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    <path d="M 58.33 74.991 c -0.06 0 -0.118 -0.002 -0.179 -0.005 c -1.653 -0.097 -2.916 -1.517 -2.819 -3.171 l 2.474 -42.244 c 0.097 -1.655 1.508 -2.933 3.171 -2.819 c 1.653 0.097 2.916 1.516 2.819 3.17 l -2.474 42.245 C 61.229 73.761 59.906 74.991 58.33 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    <path d="M 31.669 74.991 c -1.577 0 -2.898 -1.23 -2.992 -2.824 l -2.473 -42.245 c -0.097 -1.654 1.165 -3.073 2.819 -3.17 c 1.646 -0.111 3.073 1.165 3.17 2.819 l 2.473 42.244 c 0.097 1.654 -1.165 3.074 -2.819 3.171 C 31.788 74.989 31.729 74.991 31.669 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    <path d="M 45 74.991 c -1.657 0 -3 -1.343 -3 -3 V 29.747 c 0 -1.657 1.343 -3 3 -3 c 1.657 0 3 1.343 3 3 v 42.244 C 48 73.648 46.657 74.991 45 74.991 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(226,0,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                </g>
+                            </svg> -->
+                        </form>`;                        
     cartShopBox.innerHTML = cartBoxContent;
     cartItems.append(cartShopBox);
     cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
@@ -352,8 +367,11 @@ function updateTotal(){
     for (var i = 0; i < cartBoxes.length; i++){
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        // console.log(priceElement);
+        var price = priceElement.getElementsByClassName('cart-price-amount')[0].innerHTML;
+        // console.log(price);
         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-        var price = parseFloat(priceElement.innerText.replace(" р.", ""));
+        // var price = parseFloat(priceElement.innerText.replace(" р.", ""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
     }
--- js/control.js
+++ js/control.js
@@ -1,10 +1,12 @@
 $(function(){
     "use strict";
+    //Левая панель
     $('.title-box').click(function(){
         $(this).toggleClass('open');
         $(this).next('.list-link').toggleClass('open');
     })
 
+    //Footer
     // $('.f-title-box').click(function(){
     $('.f-title-box').click(function(event){
         var target = $(event.target);
@@ -15,20 +17,102 @@ $(function(){
     // $('.f-title-box').click(function(){
         $(this).toggleClass('open');
         $(this).next('.f-list-link').toggleClass('open');
-    })
- 
+    })   
+    
 
 }); 
 
 $(document).ready(function(){
+    /*Slider главной страницы */
     $('.slider-carousel').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
+        prevArrow: '.slider-button-left',
+        nextArrow: '.slider-button-right',
         dots: true,
         autoplay: true,
         autoplaySpeed: 5000,
       });
+
+      /*Slider страницы продукта */
+      $('.product-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '.slider-button-left',
+        nextArrow: '.slider-button-right',
+        dots: true,
+        asNavFor: '.slider-nav',
+        infinite: false
+      });
+
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '.slider-nav-button-left',
+        nextArrow: '.slider-nav-button-right',
+        dots: false,
+        //centerMode: true,
+        // centerPaddinsg: '0px',
+        asNavFor: '.product-carousel',
+        focusOnSelect: true,
+        // // infinite: false
+        // vertical: false,
+        // verticalSwiping: false
+        infinite: false
+      });
+
+       /*Slider страницы коллекции/категории */
+       $('.slider-carousel-collection').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '.slider-button-left',
+        nextArrow: '.slider-button-right',
+        dots: true,
+        infinite: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      /* $('.link-image').magnificPopup({
+        // delegate: '.',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+      }); */
+
+     
+
     });
 
 
